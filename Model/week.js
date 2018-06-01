a55842e504a7ed2f4cd7350e5bd481c5882d8c01
Paweł Banasik <pawelbanasik1@gmail.com>
2018-06-01 20:22:36 +0200

    create some dirs

---
+++ Model/week.js
@@ -0,0 +1,41 @@
+function WorkDay() {
+    var D_demand = 3;
+    var E_demand = 3;
+    var L_demand = 3;
+    var N_demand = 1;
+
+
+
+    function getD_demand() {
+      return this.D_demand;
+    }
+    function getE_demand() {
+      return this.E_demand;
+    }
+    function getL_demand() {
+      return this.L_demand;
+    }
+    function getN_demand() {
+      return this.N_demand;
+    }
+  }
+  function Weekend() {
+    var D_demand = 2;
+    var E_demand = 2;
+    var L_demand = 2;
+    var N_demand = 1;
+    
+
+    function getD_demand() {
+      return this.D_demand;
+    }
+    function getE_demand() {
+      return this.E_demand;
+    }
+    function getL_demand() {
+      return this.L_demand;
+    }
+    function getN_demand() {
+      return this.N_demand;
+    }
+  }
